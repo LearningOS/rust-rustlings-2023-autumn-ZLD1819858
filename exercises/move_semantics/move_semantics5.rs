af49a52f8c77fbc1d0065f3138e22273e365c2ca
zhangluodong <zhangluodong@rt-thread.com>
2023-10-09 14:24:24 +0800

    2023-10-09 a.m. completion: 40%

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,15 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
+/*
+违反了rust的借用规则，一个值同一时间只能存在一个可变引用。
+ */
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
